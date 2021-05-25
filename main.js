9fa736d1cbc244afe36e5d98c3c27ac7bbb79022
Makhmudjon1997 <79635906+Makhmudjon1997@users.noreply.github.com>
2021-05-25 20:30:19 +0500

    update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 //const filterOption = document.querySelector('.filter-todo')
-
+var createdTime;
 
 //Event Listeners
 
@@ -16,48 +16,52 @@ todoList.addEventListener('click', deleteCheck);
 function addToDo(event){
     //Prevent form from submitting
     event.preventDefault();
-
+    
     //toDo DIV
     const toDoDiv = document.createElement("div");
     toDoDiv.classList.add("todo");
-
+    
     //create li
     const newToDo = document.createElement("li");
     newToDo.innerText = todoInput.value;
     newToDo.classList.add("todo-item");
     newToDo.style.overflow = 'hidden'
-
+    
     toDoDiv.appendChild(newToDo);
-
+    
     //Check Mark button
-
+    
     const completedButton = document.createElement("button");
     completedButton.innerHTML = `<i class = "fas fa-check"></i>`;
     completedButton.classList.add("complete-btn");
     
     toDoDiv.appendChild(completedButton);
-
-      //Check Trash button
-
-      const trashButton = document.createElement("button");
-      trashButton.innerHTML = `<i class = "fas fa-trash"></i>`;
-      trashButton.classList.add("trash-btn");
-      
-      toDoDiv.appendChild(trashButton);
-
-
-      //Append to List
-
-      todoList.appendChild(toDoDiv)
-
-      //clear todoInput.value
-
-      todoInput.value = '';
+    
+    //Check Trash button
+    
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class = "fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    
+    toDoDiv.appendChild(trashButton);
+    
+    
+    //Append to List
+    
+    todoList.appendChild(toDoDiv)
+    
+    //clear todoInput.value
+    
+    todoInput.value = '';
+    
+    //adding dataset to the input
+    
+    createdTime =  todoList.dataset.createdTime = Date.now();
 }
 
 function deleteCheck(e){
     const item = e.target;
-
+    //const createdTime = todoList.dataset.createdTime = Date.now();
     //Delete todo
 
     if(item.classList[0] === 'trash-btn'){
@@ -74,9 +78,10 @@ function deleteCheck(e){
 
     if(item.classList[0] === 'complete-btn'){
         const todo = item.parentElement;
-        todo.classList.toggle('completed')
+        todo.classList.toggle('completed');
+        alert(`You spend ${Math.round((Date.now() - createdTime) / 1000)} seconds to complete this task`)
     }
-
+    
 }
 
 
